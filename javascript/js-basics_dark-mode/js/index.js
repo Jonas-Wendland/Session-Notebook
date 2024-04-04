6eba0f47d18b7d4a82a3a19eb2dfa56bd67d21c9
Jonas Wendland <wendjon@gmx.de>
2024-04-04 12:14:11 +0200

    js-basics_dark-mode done

--- javascript/js-basics_dark-mode/js/index.js
+++ javascript/js-basics_dark-mode/js/index.js
@@ -1,3 +1,16 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+const darkmode = document.querySelector('[data-js="dark-mode-button"]');
+const lightmode = document.querySelector('[data-js="light-mode-button"]');
+const togglemode = document.querySelector('[data-js="toggle-button"]');
+
+darkmode.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+});
+lightmode.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+});
+togglemode.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
