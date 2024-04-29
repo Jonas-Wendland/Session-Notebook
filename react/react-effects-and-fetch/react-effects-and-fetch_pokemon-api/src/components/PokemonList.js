8f9716e64e2c89957fdf93277fb1aa423d6a1bd8
Jonas Wendland <wendjon@gmx.de>
2024-04-29 12:55:46 +0200

    react effects and fetch challanges

---
+++ react/react-effects-and-fetch/react-effects-and-fetch_pokemon-api/src/components/PokemonList.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+export default function PokemonList() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    async function loadPokemon() {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+      const data = await response.json();
+      setPokemon(data.results);
+    }
+    loadPokemon();
+  }, []);
+
+  return (
+    <main>
+      <ul>
+        {pokemon.map(({ name }) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+    </main>
+  );
+}
